cc9e5350230537878334503d5f561d065480a718
Tushar-Kanvinde <tskanvinde@gmail.com>
2018-01-23 11:24:42 +0530

    checking another way of calling the plugin

--- www/sahakarfingerprint.js
+++ www/sahakarfingerprint.js
@@ -1,5 +1,26 @@
-var exec = require('cordova/exec');
+//var exec = require('cordova/exec');
 
-exports.getFingerPrint = function (arg0, success, error) {
-    exec(success, error, 'sahakarfingerprint', 'getFingerPrint', [arg0]);
-};
+//exports.getFingerPrint = function (arg0, success, error) {
+//    exec(success, error, 'sahakarfingerprint', 'getFingerPrint', [arg0]);
+//};
+
+
+
+//var exec = require('cordova/exec');
+
+//var sahakarfingerprint = {
+//    getFingerPrint: function(arg0) {
+//        exec(null, null, "sahakarfingerprint", "getFingerPrint", [arg0]);
+//    }
+//};
+
+//module.exports = sahakarfingerprint;
+
+window.getFingerPrint=function(success,failure,text){
+    cordova.exec(success,
+             failure,
+             "sahakarfingerprint",
+             "getFingerPrint",
+             [text]);    
+}
+
